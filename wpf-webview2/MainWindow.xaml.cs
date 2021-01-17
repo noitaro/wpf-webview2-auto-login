00bc2ebc40c44779f01e26130b952841faeb9f52
noitaro <noitaro@example.com>
2021-01-17 18:13:47 +0900

    first commit

---
+++ wpf-webview2/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace wpf_webview2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // IDを自動入力する.
+            webView.CoreWebView2.ExecuteScriptAsync($"document.querySelector('#LoginComponent > form > div.input-field-group > div:nth-child(1) > input[type=text]').value = '★★★ ID ★★★'");
+            // パスワードを自動入力する.
+            webView.CoreWebView2.ExecuteScriptAsync($"document.querySelector('#LoginComponent > form > div.input-field-group > div:nth-child(2) > input[type=password]').value = '★★★ パスワード ★★★'");
+            // ログインをする.
+            webView.CoreWebView2.ExecuteScriptAsync($"document.querySelector('#LoginComponent > form > button').click()");
+        }
+    }
+}
